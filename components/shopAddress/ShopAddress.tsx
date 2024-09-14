8baaf8f8be2212276fcd380cd48199857dfc6850
Ali Keramat <alikeramatdev@gmail.com>
2024-09-14 15:07:19 -0700

    feat(ShopAddress): Enhanced styling, improved data handling, and minor structural changes

--- components/shopAddress/ShopAddress.tsx
+++ components/shopAddress/ShopAddress.tsx
@@ -1,23 +1,45 @@
-import { Box, Heading, ListItem, Text, UnorderedList } from "@chakra-ui/react";
+"use client";
+import {
+  Box,
+  Heading,
+  ListItem,
+  Text,
+  UnorderedList,
+  useColorMode,
+} from "@chakra-ui/react";
 
 interface propsType {
   title: string;
   data: { title: string; value: number | string }[];
 }
 
-const ShopAddress = (props: propsType) => {
+const ShopAddress = ({ title, data }: propsType) => {
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
-    <Box dir="rtl">
-      <Heading m="1rem" as="h4" size="sm">
-        {props.title}
+    <Box dir="rtl" width='28.594vw'>
+      <Heading
+        mb="24px"
+        mr="1rem"
+        as="h4"
+        size="20px"
+        fontWeight="500"
+        lineHeight="32px"
+      >
+        {title}
       </Heading>
       <UnorderedList styleType='""'>
-        {props.data.map((item, index) => (
-          <ListItem key={index} mb="0.7rem">
-            <Text display="inline-block" color="red">
+        {data.map((item, index) => (
+          <ListItem key={index} mb="16px">
+            <Text display="inline-block" color="#DB2777">
               {item.title} :&nbsp;
             </Text>
-            <Text display="inline-block">{item.value} </Text>
+            <Text
+              display="inline-block"
+              color={colorMode === "light" ? "black" : "white"}
+            >
+              {item.value}
+            </Text>
           </ListItem>
         ))}
       </UnorderedList>
