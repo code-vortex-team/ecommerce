4b38585c3fc4b3b56930ab827df5537fa8becb66
Alireza <dashtialireza77@gmail.com>
2024-09-15 00:21:20 +0330

    styling button 1 and 2

--- app/AlirezaTestPage/page.tsx
+++ app/AlirezaTestPage/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from '@chakra-ui/react'
+
 const Page = () => {
 
     return (<>
-
+    <div>
+        <Button variant={"button1"}>فروشگاه</Button>
+        <Button variant={"button2"}>مشاهده بیشتر</Button>
+    </div>
     </>)
 }
 
--- lib/chakra/theme.tsx
+++ lib/chakra/theme.tsx
@@ -1,10 +1,15 @@
 import { extendTheme, type ThemeConfig } from '@chakra-ui/react'
+import { ChakraButtonStyle } from '@/components/Styles/ChakraButtonStyle'
 
 const config: ThemeConfig = {
     initialColorMode: 'light',
     useSystemColorMode: false,
 }
 
-const theme = extendTheme({ config })
+const theme = extendTheme({ config,
+    components: {
+        Button: ChakraButtonStyle,
+    }
+ })
 
 export default theme
