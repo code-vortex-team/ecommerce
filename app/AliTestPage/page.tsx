47afe7050f29000ff017692bab4b2cf28547d5ca
Ali Keramat <alikeramatdev@gmail.com>
2024-09-16 15:00:16 -0700

    feat(added orderDetails component and fix some bugs)

--- app/AliTestPage/page.tsx
+++ app/AliTestPage/page.tsx
@@ -1,8 +1,17 @@
+import OrderDetails from "@/components/orderdetails/OrderDetails";
+
+const prices = {
+  subtotal: 120000,
+  trackingFee: 30000,
+  tax: 1000,
+};
+
 const Page = () => {
-
-    return (<>
-
-    </>)
-}
+  return (
+    <>
+      <OrderDetails prices={prices} payWay="درگاه پرداخت پاسارگاد" address="تهران خ آزادی نبش کوچه قنبری پلاک ۱۹۲"  />
+    </>
+  );
+};
 
 export default Page;
