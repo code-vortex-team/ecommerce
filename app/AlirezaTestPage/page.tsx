5794adae1ede937de7cba7536e21a77e6a85c891
Alireza Dashti <dashtialireza77@gmail.com>
2024-09-17 01:05:50 +0330

    feat: add ShoppingData component

--- app/AlirezaTestPage/page.tsx
+++ app/AlirezaTestPage/page.tsx
@@ -1,13 +1,26 @@
-import { Button } from '@chakra-ui/react'
 
-const Page = () => {
+import React from 'react';
+import ShoppingData from '@/components/shoppingData/ShoppingData';
+import { Button, Box } from '@chakra-ui/react';
 
-    return (<>
+const Page: React.FC = () => {
+
+  const data = [
+    { value: 'Value 1', item: 'Item 1' },
+    { value: 'Value 2', item: 'Item 2' },
+    { value: 'Value 3', item: 'Item 3' },
+  ];
+
+  return (
     <div>
-        <Button variant={"button1"}>فروشگاه</Button>
-        <Button variant={"button2"}>مشاهده بیشتر</Button>
+      <Button variant="button1">فروشگاه</Button>
+      <Button variant="button2">مشاهده بیشتر</Button>
+
+      <Box p={4}>
+        <ShoppingData title="My List" list={data} />
+      </Box>
     </div>
-    </>)
-}
+  );
+};
 
 export default Page;
---
+++ components/shoppingData/ShoppingData.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Box, Flex, Text, Heading } from '@chakra-ui/react';
+
+
+interface ListItem {
+  value: string;
+  item: string;
+}
+
+interface ItemListProps {
+  title: string;
+  list: ListItem[];
+}
+
+
+
+const ShoppingData: React.FC<ItemListProps> = ({ title, list }) => {
+    return (
+      <Box>
+        <Heading 
+        position="absolute" 
+        top={0} 
+        right={0} 
+        mb={4} 
+        textAlign="right"
+        fontWeight="medium"
+      >
+        {title}
+      </Heading>
+        {list.map((listItem, index) => (
+          <Flex
+            key={index}
+            justify="space-between" 
+            py={2} 
+            borderBottom="1px solid #e2e8f0" 
+          >
+           
+            <Text fontWeight="normal">{listItem.value}</Text>
+  
+            <Text fontWeight="bold" color="#58616C">{listItem.item}</Text>
+          </Flex>
+        ))}
+      </Box>
+    );
+  };
+
+
+export default ShoppingData;
