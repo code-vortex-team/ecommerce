280a57505bf26d7ba97aef9bed4e3d6e82b43caa
Ali Keramat <alikeramatdev@gmail.com>
2024-09-19 17:20:05 -0700

    feat(Badge): create Badge style config

--- lib/chakra/theme.tsx
+++ lib/chakra/theme.tsx
@@ -1,10 +1,12 @@
 import { extendTheme, type ThemeConfig } from '@chakra-ui/react'
+import BadgeStyleConfig from '@/components/Style/BadgeConfigStyle'
+
 
 const config: ThemeConfig = {
     initialColorMode: 'light',
     useSystemColorMode: false,
 }
 
-const theme = extendTheme({ config })
+const theme = extendTheme({ config, components : {Badge : BadgeStyleConfig} })
 
 export default theme
