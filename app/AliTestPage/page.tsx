b39ce34a28c9fb3334515490c2b9c93f2b64c7cc
Ali Keramat <alikeramatdev@gmail.com>
2024-09-21 16:09:31 -0700

    chore: prepare component for merge

--- app/AliTestPage/page.tsx
+++ app/AliTestPage/page.tsx
@@ -1,44 +1,5 @@
-import SideMenu from "@/components/sidemenu/SideMenu";
-import color from "@/components/colors"
-import { IoHomeOutline } from "react-icons/io5";
-import { RiShoppingBag4Line } from "react-icons/ri";
-import { MdOutlineShoppingCart } from "react-icons/md";
-import { MdFavoriteBorder } from "react-icons/md";
-import { Box } from "@chakra-ui/react";
-
-const sidemenulist = [
-  {
-    title: "داشبورد",
-    pathName: "/dashboard",
-    icon: <IoHomeOutline />,
-    notif: null,
-  },
-  {
-    title: "فروشگاه",
-    pathName: "/store",
-    icon: <RiShoppingBag4Line />,
-    notif: null,
-  },
-  {
-    title: "سبدخرید",
-    pathName: "/shopping-cart",
-    icon: <MdOutlineShoppingCart />,
-    notif: 1,
-  },
-  {
-    title: "علاقه مندی ها",
-    pathName: "/favorite",
-    icon: <MdFavoriteBorder />,
-    notif: null,
-  },
-];
-
 const Page = () => {
-  return (
-    <Box>
-      <SideMenu list={sidemenulist} />
-    </Box>
-  );
+  return <></>;
 };
 
 export default Page;
