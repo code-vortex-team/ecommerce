1ac09178af35dd92f02219b90c73cb06b6f2bae2
pouria sharifi <sharifi751997@gmail.com>
2024-09-22 00:24:19 +0330

    feat(color): create chakra color palette theme config

--- lib/chakra/theme.tsx
+++ lib/chakra/theme.tsx
@@ -1,10 +1,26 @@
-import { extendTheme, type ThemeConfig } from '@chakra-ui/react'
+import {extendTheme, type ThemeConfig, ThemeOverride} from '@chakra-ui/react'
+import {color} from "@/components/colors";
+import {ColorVariable, replaceDotsWithVar} from "@/components/styles/ChakraColorStyle";
 
 const config: ThemeConfig = {
     initialColorMode: 'light',
     useSystemColorMode: false,
 }
 
-const theme = extendTheme({ config })
+
+const ThemeConfig: ThemeOverride = {
+    styles: {
+        global: (props) => ({
+            ...ColorVariable(props)
+        }),
+    },
+    colors: {
+        ...replaceDotsWithVar(color),
+    },
+
+}
+
+
+const theme = extendTheme({config, ...ThemeConfig})
 
 export default theme
