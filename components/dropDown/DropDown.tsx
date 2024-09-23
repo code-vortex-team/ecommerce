21fdb445e7353c29324673e758cf64e139922a53
Alireza Dashti <dashtialireza77@gmail.com>
2024-09-24 00:21:58 +0330

    fix(theme): fixing dropdown

--- components/dropDown/DropDown.tsx
+++ components/dropDown/DropDown.tsx
@@ -3,6 +3,7 @@ import {Menu, MenuButton, MenuItem, MenuList} from "@chakra-ui/menu";
 import {Box, Flex} from "@chakra-ui/react";
 import React from "react";
 import {useRouter} from "next/navigation";
+import {color} from "@/components/colors";
 
 interface IDropDown {
     title: string;
@@ -22,7 +23,7 @@ const DropDown: React.FC<IDropDown> = ({title, list}) => {
                         <Flex alignItems={"center"} gap={2}>
                             <Box sx={{
                                 fontWeight: "400",
-                                fontSize: "16px",
+                                fontSize: "1rem",
                             }}>
                                 {title}
                             </Box>
@@ -30,23 +31,23 @@ const DropDown: React.FC<IDropDown> = ({title, list}) => {
                                 <svg width="12" height="7" viewBox="0 0 12 7" fill="none"
                                      xmlns="http://www.w3.org/2000/svg">
                                     <path d="M10.6666 0.999959L5.99996 5.66663L1.33329 0.999959" stroke="black"
-                                          stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/>
+                                          strokeWidth="1.33333" strokeLinecap="round" strokeLinejoin="round"/>
                                 </svg>
 
                             </Box>
                         </Flex>
 
                     </MenuButton>
-                    <MenuList minWidth={123}
+                    <MenuList minWidth="10.5rem"
                               sx={{
-                                  bgColor: "#151515",
+                                  bgColor: color.base.slide,
                                   borderColor: "#3F4043",
                                   fontWeight: "400",
-                                  fontSize: "16px",
+                                  fontSize: "1rem",
                                   button: {
-                                      borderRadius: "4px",
+                                      borderRadius: "0.25rem",
                                       fontWeight: "400",
-                                      fontSize: "16px",
+                                      fontSize: "1rem",
                                       bg: "#151515",
                                       color: "#FFFFFF",
                                       _hover: {
@@ -58,18 +59,19 @@ const DropDown: React.FC<IDropDown> = ({title, list}) => {
 
                         {list.map((item) => (
                             <MenuItem key={item.url} sx={{
-                                width: "153px",
-                                height: "37px",
-                                paddingTop: "8px",
-                                paddingBottom: "0px",
-                                gap: "10px",
-                                borderRadius: "4px 0 0 0",
+                                width: "9.5625rem",
+                                height: "2.3125rem",
+                                paddingTop: "0.5rem",
+                                paddingBottom: "0",
+                                marginX: "0.5rem",
+                                gap: "0.625rem",
+                                borderRadius: "0.25rem 0 0 0",
                                 opacity: 1,
                                 transition: "all 0.3s",
                                 _hover: {
-                                    marginX: "8px",
+
                                     opacity: 1,
-                                    bgColor: "#3F4043",
+                                    bgColor: "#DB2777",
                                     color: "#DB2777",
                                 },
                             }} onClick={() => router.push(item.url)}>
