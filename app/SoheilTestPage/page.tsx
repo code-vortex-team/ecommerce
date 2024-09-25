b19b700eab07d98a2fbbca631d13f9963d2b1b49
Soheil-LGH <soheilghashghaei@gmail.com>
2024-09-26 03:23:25 +0330

    fix(testpage and home cleaned)

--- app/SoheilTestPage/page.tsx
+++ app/SoheilTestPage/page.tsx
@@ -1,21 +1,8 @@
-"use client";
-import FilterCategory from "@/components/filterCategory/FilterCategory";
-import { Button, Input, useColorMode } from "@chakra-ui/react";
-
 const Page = () => {
-  const { colorMode, toggleColorMode } = useColorMode();
-  const title = "فیلتر دسته‌بندی";
-  const categories = [
-    { name: "Smartphones", active: false },
-    { name: "Laptop", active: false },
-    { name: "Tablets", active: false },
-    { name: "Wearables", active: false },
-    { name: "Audio", active: false },
-  ];
-  return (
-    <>
-    </>
-  );
-};
+
+  return (<>
+
+  </>)
+}
 
 export default Page;
