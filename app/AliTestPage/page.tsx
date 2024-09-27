3aa4d31d87499b15164c326db5f62ddbc9993f42
Ali Keramat <alikeramatdev@gmail.com>
2024-09-27 04:23:18 -0700

    fix(sidemenu): Removed Framer Motion to enhance the performance of the side menu component. Adjusted z-index and top CSS properties to ensure proper rendering and fix any potential layout bugs. Additionally, added the children prop.

--- app/AliTestPage/page.tsx
+++ app/AliTestPage/page.tsx
@@ -1,32 +1,59 @@
-import ShopAddress from "@/components/shopAddress/ShopAddress";
-import { Badge, Box } from "@chakra-ui/react";
-import OrderDetails from "@/components/orderdetails/OrderDetails";
+import SideMenu from "@/components/sidemenu/SideMenu";
+import { Box } from "@chakra-ui/react";
+import { title } from "process";
+// import ProductCard from "@/components/product-card/ProductCard";
 
-const information = [
-  { title: " شماره سفارش", value: 224568 },
-  { title: "نام", value: "علی کرامت" },
-  { title: "ایمیل", value: "Example@example.com" },
-  { title: "آدرس", value: "تهران" },
-  { title: "روش پرداخت", value: "به پرداخت ملت" },
+// const productInformation = {
+//   title: "Apple iPhone 14 Pro",
+//   price: '10,000',
+//   content:
+//     "آیفون 14 پرو دارای صفحه نمایش 6.1 اینچی سوپر است نمایشگر Retina XDR ...",
+//   badge: "Apple",
+// };
+
+// const source =
+//   "https://s3-alpha-sig.figma.com/img/3dc0/c069/7ad08037bec720feec0056d30cd179d9?Expires=1728259200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=RuU6DTNQTSTF3yfVTk9mHRy5eUhIari0DGGX~~Rh~pTnIRua~0XQRXuVZcnOqVAoONtKhSdTvTXhLpBmGBlz-3754KjnV0fKlcNV4QgcFF2LsZ65n6lhMz7gRbH-wj5pd6BNcG8WFn5Px3DvvR1j8q4lhYBO5a5IOHgNtiBs2vTjzjzBB27urc8AKyI7Czvohd6HWbMF8JrTvNBnMU~IYdbZiSb5kDQFaWyluXdp99MjjVqWLJ9DI-ubM~Hny9RnM010kdSLEF6HFU1VCOSPEvK9Rn3NHuyNayDtJ40iIaM3i2NHQssF4ILqdQ3FRH~67RTbJW8dmds5CIAH2xDD0A__";
+
+import { IoHomeOutline } from "react-icons/io5";
+import { RiShoppingBag4Line } from "react-icons/ri";
+import { MdOutlineShoppingCart } from "react-icons/md";
+import { MdFavoriteBorder } from "react-icons/md";
+
+const sidemenulist = [
+  {
+    title: "داشبورد",
+    pathName: "/dashboard",
+    icon: <IoHomeOutline />,
+    notif: null,
+  },
+  {
+    title: "فروشگاه",
+    pathName: "/store",
+    icon: <RiShoppingBag4Line />,
+    notif: null,
+  },
+  {
+    title: "سبدخرید",
+    pathName: "/shopping-cart",
+    icon: <MdOutlineShoppingCart />,
+    notif: 1,
+  },
+  {
+    title: "علاقه مندی ها",
+    pathName: "/favorite",
+    icon: <MdFavoriteBorder />,
+    notif: null,
+  },
 ];
-
-const prices = {
-  subtotal: 120000,
-  trackingFee: 30000,
-  tax: 1000,
-};
 
 const Page = () => {
   return (
     <Box>
-        <OrderDetails prices={prices} payWay="درگاه پرداخت پاسارگاد" address="تهران خ آزادی نبش کوچه قنبری پلاک ۱۹۲"  />
-      <ShopAddress title="آدرس دریافت" data={information} />
-      <Badge variant='pink'>test</Badge>
-      <Badge variant='pinkLg'>test large</Badge>
+      {/* <ProductCard _id='1'image = {source} description={productInformation.content} price= {productInformation.price} name={productInformation.title} category={productInformation.badge} /> */}
+      <SideMenu list={sidemenulist}/>
     </Box>
   );
 };
 
-
-
 export default Page;
+
