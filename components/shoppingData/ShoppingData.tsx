256253ce4fa7ae60949ea49bda65f62e3acbfb06
Alireza Dashti <dashtialireza77@gmail.com>
2024-09-29 01:03:39 +0330

    fix(style): fixing the shopping data component

--- components/shoppingData/ShoppingData.tsx
+++ components/shoppingData/ShoppingData.tsx
@@ -1,48 +1,51 @@
 import React from 'react';
-import { Box, Flex, Text, Heading } from '@chakra-ui/react';
+import {Box, Flex, Heading, Text} from '@chakra-ui/react';
+import {color} from "@/components/colors";
 
 
 interface ListItem {
-  value: string;
-  item: string;
+    value: string;
+    item: string;
 }
 
 interface ItemListProps {
-  title: string;
-  list: ListItem[];
+    title: string;
+    list: ListItem[];
 }
 
 
-
-const ShoppingData: React.FC<ItemListProps> = ({ title, list }) => {
+const ShoppingData: React.FC<ItemListProps> = ({title, list}) => {
     return (
-      <Box>
-        <Heading 
-        position="absolute" 
-        top={0} 
-        right={0} 
-        mb={4} 
-        textAlign="right"
-        fontWeight="medium"
-      >
-        {title}
-      </Heading>
-        {list.map((listItem, index) => (
-          <Flex
-            key={index}
-            justify="space-between" 
-            py={2} 
-            borderBottom="1px solid #e2e8f0" 
-          >
-           
-            <Text fontWeight="normal">{listItem.value}</Text>
-  
-            <Text fontWeight="bold" color="#58616C">{listItem.item}</Text>
-          </Flex>
-        ))}
-      </Box>
+        <Box sx={{
+            width: "34.3rem",
+            height: "12.5rem"
+        }}>
+
+            <Heading
+                mb="1.5rem"
+                textAlign="right"
+                fontWeight={500}
+                fontSize="20px"
+            >
+                {title}
+            </Heading>
+            {list.map((listItem, index) => (
+                <Flex
+                    key={index}
+                    justify="space-between"
+                    pb={4}
+
+                >
+
+                    <Text fontSize="1rem" fontWeight={700}
+                          sx={{color: color.text.secondary}}>{listItem.value} : </Text>
+
+                    <Text fontSize="1rem" fontWeight={400} sx={{color: color.text.primary}}>{listItem.item}</Text>
+                </Flex>
+            ))}
+        </Box>
     );
-  };
+};
 
 
 export default ShoppingData;
