131bd305935e22fd4c6d5157e3741525d0803641
Soheil-LGH <soheilghashghaei@gmail.com>
2024-09-30 18:35:05 +0330

    fix(style edited, container tag used)

--- app/(user)/user/cart/checkout/page.tsx
+++ app/(user)/user/cart/checkout/page.tsx
@@ -1,14 +1,9 @@
 "use client"
 import React from "react";
 import ShopOrder from "@/components/shopOrder/CreateTable";
-import { Box, Button, Img, Text } from "@chakra-ui/react";
+import { Button, Container, Img, Text } from "@chakra-ui/react";
 import { color } from "@/components/colors";
-import SideMenu from "@/components/sidemenu/SideMenu";
 import { FC } from "react";
-import { HiOutlineHome } from "react-icons/hi";
-import { RiShoppingBagLine } from "react-icons/ri";
-import { AiOutlineShoppingCart } from "react-icons/ai";
-import { FaHeart } from "react-icons/fa";
 import ShopAddress from "@/components/shopAddress/ShopAddress";
 import ShoppingData from "@/components/shoppingData/ShoppingData";
 
@@ -65,53 +60,56 @@ const data = [
   },
 ];
 
-const sideMenuList = [
-    {
-      title: "home",
-      pathName: "string",
-      icon: <HiOutlineHome />,
-      notif: null,
-    }, 
-    {
-      title: "shoppingBag",
-      pathName: "string",
-      icon: <RiShoppingBagLine />,
-      notif: null,
-    },
-    {
-      title: "shoppingCart",
-      pathName: "string",
-      icon: <AiOutlineShoppingCart />,
-      notif: 2,
-    },
-    {
-      title: "lovedItems",
-      pathName: "string",
-      icon: <FaHeart />,
-      notif: null,
-    }
-  ]
-
   const shopAddressTitle = "آدرس دریافت";
   const shopAddressData = [{title: "شماره سفارش", value: 235465}, {title: "نام", value: "علی"}, {title: "ایمیل", value: "myemail@gmail.com"}];
   const shoppingDataTitle = "خلاصه خرید"
   const shoppingDataList = [{value: "قیمت محصول", item: "10000 تمومان"}, {value: "هزینه ارسال", item: "1000 تومان"}]
 
   const App: FC = () => (
-    <Box bg={color.base.background}  height={"100vh"}>
-      <aside >
-        <SideMenu list={sideMenuList} />
-      </aside>
-      <main style={{paddingRight: "140px", paddingLeft: "60px", paddingTop: "40px", display:"flex"}}>
-        <Box width={"max-content"} border={"1px solid"} borderColor={color.base.textFieldStroke} marginLeft={"30px"}>
-        <ShopOrder columns={columns} data={data} />
-        </Box>
-        <Box width={"400px"}>
-            <ShopAddress title={shopAddressTitle} data={shopAddressData}/>
-            <ShoppingData title={shoppingDataTitle} list={shoppingDataList}/>
-        </Box>
+      <main
+        style={{
+          paddingRight: "100px",
+          paddingLeft: "60px",
+          paddingTop: "60px",
+          display: "flex",
+        }}
+      >
+        <Container
+          maxW={"3xl"}
+          border={"1px solid"}
+          borderColor={color.base.textFieldStroke}
+        >
+          <ShopOrder columns={columns} data={data} />
+        </Container>
+        <Container marginRight={"15px"}>
+          <ShopAddress title={shopAddressTitle} data={shopAddressData} />
+          <Text
+            width={"auto"}
+            bg={color.base.card}
+            padding={"5px 10px"}
+            margin={"20px 0"}
+            border={"1px solid"}
+            borderRadius={"4px"}
+            borderColor={color.base.textFieldStroke}
+            textColor={color.text.primary}
+            fontWeight={"600"}
+            fontSize={"16px"}
+          >
+            Status
+          </Text>
+          <ShoppingData title={shoppingDataTitle} list={shoppingDataList} />
+          <Button
+            width={"100%"}
+            marginTop={"20px"}
+            bg={color.primary.main}
+            borderRadius={"full"}
+            color={color.text.button}
+            _hover={{bg:color.primary.light}}
+          >
+            پرداخت
+          </Button>
+        </Container>
       </main>
-    </Box>
   );
 
 export default App;
