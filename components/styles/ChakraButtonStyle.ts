172822ef6141096abf2ba656a1f41fe843e93d37
Alireza Dashti <dashtialireza77@gmail.com>
2024-10-01 12:27:34 +0330

    fix(style): fixing the button

--- components/styles/ChakraButtonStyle.ts
+++ components/styles/ChakraButtonStyle.ts
@@ -1,38 +1,38 @@
-import { defineStyleConfig } from '@chakra-ui/react';
+import {defineStyleConfig} from '@chakra-ui/react';
 
 export const ChakraButtonStyle = defineStyleConfig({
-  
-  variants: {
-    button1: {
-      bg: '#DB2777',
-      color: 'white',
-      minWidth: '8.4375rem', 
-      height: '3rem',     
-      padding: '0.5rem 2rem', 
-      gap: '0rem',        
-      borderRadius: '62.5rem', 
-      _hover: {
-        bg: '#831747',   
-        color: 'white', 
-      },
-    },
-    button2: {
-      bg: '#DB2777',
-      color: 'white',
-      minWidth: '8.25rem',  
-      height: '2.25rem',
-      padding: '0.5rem 0.75rem', 
-      gap: '0rem',     
-      borderRadius: '0.5rem', 
-      _hover: {
-        bg: '#831747',   
-        color: 'white', 
-      },
+
+    variants: {
+        roundedPinkButton: {
+            bg: '#DB2777',
+            color: 'white',
+            minWidth: '8.4375rem',
+            height: '3rem',
+            padding: '0.5rem 2rem',
+            gap: '0rem',
+            borderRadius: '62.5rem',
+            _hover: {
+                bg: '#831747',
+                color: 'white',
+            },
+        },
+        regularPinkButton: {
+            bg: '#DB2777',
+            color: 'white',
+            minWidth: '8.25rem',
+            height: '2.25rem',
+            padding: '0.5rem 0.75rem',
+            gap: '0rem',
+            borderRadius: '0.5rem',
+            _hover: {
+                bg: '#831747',
+                color: 'white',
+            },
+        },
+
+
     },
 
-    
-  },
- 
 });
 
 
