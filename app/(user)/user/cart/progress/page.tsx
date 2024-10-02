8809b6a170f7130b9cb2cfaa578ef60b02985303
pouria sharifi <sharifi751997@gmail.com>
2024-10-02 21:23:36 +0330

    fix(cart):cart progress create page

--- app/(user)/user/cart/progress/page.tsx
+++ app/(user)/user/cart/progress/page.tsx
@@ -1,6 +1,43 @@
-const Page = () => {
+"use client"
+import {Box, Container} from "@chakra-ui/react";
+import BasicStepper from "@/components/UI/Stepper/BasicStepper";
+import {useState} from "react";
+import GetAddressForm from "@/components/Forms/getAddressForm";
+
+const Page = (props) => {
+    const [index, setIndex] = useState(1)
+
+
+    const list = [
+        {
+            title: "ورود"
+        },
+        {
+            title: "آدرس"
+        },
+        {
+            title: "خلاصه خرید"
+        },
+    ]
+
 
     return (<>
+
+        <Container maxW={"2xl"} py={5}>
+            <Box dir={"ltr"}>
+                <BasicStepper activeIndex={index} list={list}></BasicStepper>
+            </Box>
+
+            <Box pt={10}>
+                {index === 1 &&
+                    <GetAddressForm onSubmit={() => {
+                        setIndex(2)
+                    }}/>
+                }
+
+            </Box>
+        </Container>
+
 
     </>)
 }
