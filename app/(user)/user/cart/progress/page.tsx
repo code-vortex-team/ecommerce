bb68eaa2874349c61f5ba363e0cb6916572be8e4
pouria sharifi <sharifi751997@gmail.com>
2024-10-03 20:49:59 +0330

    feat(page):create progress page

--- app/(user)/user/cart/progress/page.tsx
+++ app/(user)/user/cart/progress/page.tsx
@@ -1,11 +1,23 @@
 "use client"
-import {Box, Container} from "@chakra-ui/react";
+import {Box, Button, Container, Img} from "@chakra-ui/react";
 import BasicStepper from "@/components/UI/Stepper/BasicStepper";
-import {useState} from "react";
+import React, {useEffect, useState} from "react";
 import GetAddressForm from "@/components/Forms/getAddressForm";
+import ShopOrder from "@/components/shopOrder/CreateTable";
+import {useAppDispatch, useAppSelector} from "@/lib/redux/hooks";
+import {OrdersApi, ProductsApi} from "@/lib/openapi/generated-client";
+import {addItem} from "@/lib/redux/features/Basket/basketSlice";
+import OrderDetails from "@/components/orderdetails/OrderDetails";
 
 const Page = (props) => {
     const [index, setIndex] = useState(1)
+    const [formData, setFormData] = useState<unknown>({})
+    const {
+        list: data,
+        totalPrice
+    } = useAppSelector(s => s.basket)
+
+    const dispatch = useAppDispatch()
 
 
     const list = [
@@ -21,8 +33,73 @@ const Page = (props) => {
     ]
 
 
-    return (<>
+    const columns = [
+        {
+            id: "image",
+            header: () => "عکس",
+            accessorKey: "image",
+            cell: (info: any) => <Img src={info.getValue()} alt="product image" minWidth={"64px"} boxSize={"64px"}
+                                      marginInline={"auto"}/>,
+        },
+        {
+            id: "name",
+            header: () => "نام محصول",
+            accessorKey: "name",
+        },
+        {
+            id: "count",
+            header: () => "تعداد",
+            accessorKey: "count",
+        },
+        {
+            id: "price",
+            header: () => "قیمت",
+            accessorKey: "price",
+        },
+        {
+            id: "count",
+            header: () => "قیمت نهایی",
+            accessorKey: "count",
+            cell: (e) => {
+                const price = e.row.getValue("price")
+                const count = e.row.getValue("count")
 
+                return (<>{price * count}</>)
+            }
+        },
+
+    ];
+
+
+    useEffect(() => {
+        new ProductsApi().apiProductsAllproductsGet().then((r: any) => {
+            r.data?.map(item => {
+                dispatch(addItem({
+                    ...item,
+                }))
+            })
+        })
+    }, []);
+
+    const createOrder = () => {
+        new OrdersApi().apiOrdersPost({
+            orderItems: data.map(item => ({
+                _id: item._id,
+                name: item.name,
+                quy: item.count
+            })),
+            paymentMethod: "Cash",
+            shippingAddress: {
+                "address": formData.address,
+                "city": formData.city,
+                "postalCode": formData.zipcode
+            }
+        }).then((r) => {
+            console.log(r)
+        })
+    }
+
+    return (<>
         <Container maxW={"2xl"} py={5}>
             <Box dir={"ltr"}>
                 <BasicStepper activeIndex={index} list={list}></BasicStepper>
@@ -30,14 +107,44 @@ const Page = (props) => {
 
             <Box pt={10}>
                 {index === 1 &&
-                    <GetAddressForm onSubmit={() => {
+                    <GetAddressForm onSubmit={(data) => {
                         setIndex(2)
+                        setFormData({
+                            address: `
+                            ${data.country} ${data.city} ${data.address} ${data.zipcode}
+                            `,
+                            payWay: "درگاه پرداخت",
+                            prices: {
+                                tax: 10000,
+                                subtotal: totalPrice,
+                                trackingFee: 10000,
+
+                            },
+                        })
                     }}/>
                 }
+
 
             </Box>
         </Container>
 
+        <Container maxW={"100%"}>
+            {index === 2 &&
+                <>
+                    <Box>
+                        <ShopOrder data={data} columns={columns}/>
+                    </Box>
+
+                    <Box>
+                        <OrderDetails  {...formData}  />
+                        <Box pt={5}>
+                            <Button variant="roundedPinkButton" minWidth={"100%"} onClick={createOrder}>ثبت
+                                سفارش</Button>
+                        </Box>
+                    </Box>
+                </>
+            }
+        </Container>
 
     </>)
 }
