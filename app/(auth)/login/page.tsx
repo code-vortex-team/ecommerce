dfffbc66d6f1115ef6a7f81e68adf624d1e6a1ec
pouria sharifi <sharifi751997@gmail.com>
2024-10-04 20:18:55 +0330

    feat(api) auth withCredentials

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,13 +1,15 @@
 'use client';
-import { useForm } from "react-hook-form";
-import { Button, FormControl, FormErrorMessage, FormLabel, Input, Link, Text } from "@chakra-ui/react";
-import NextLink from "next/link";
-import { UserApi } from "@/lib/openapi/generated-client";
-import { color } from "@/components/colors";
-import { useState } from "react";
+import {useForm} from "react-hook-form";
+import {Button, FormControl, FormErrorMessage, FormLabel, Input, Link, Text} from "@chakra-ui/react";
+import {UserApi} from "@/lib/openapi/apiClient";
+import {color} from "@/components/colors";
+import {ReactNode, useState} from "react";
 
 const Page = () => {
-    const { register, formState: { errors }, handleSubmit } = useForm<{ email: string; password: string; }>();
+    const {register, formState: {errors}, handleSubmit}: ReactNode | unknown = useForm<{
+        email: string;
+        password: string;
+    }>();
     const [isLoading, setIsLoading] = useState(false);
 
     const onSubmit = (data) => {
@@ -17,8 +19,8 @@ const Page = () => {
             "password": data.password
         })
             .then((r) => {
-                alert("تو تونستی وارد بشی");
                 console.log(r.data, "دیتات اینه");
+
             })
             .catch((e) => {
                 alert("تو نتونستی وارد بشی متاسفم!");
@@ -40,7 +42,7 @@ const Page = () => {
                     <FormLabel mt='2rem' mb='0.5rem' color="text.primary" fontSize='1rem'>ایمیل</FormLabel>
                     <Input placeholder={"ایمیل خود را وارد نمایید"} color={color.text.secondary} fontSize='1rem'
                            bg={color.base.textField} borderColor={color.base.textFieldStroke}
-                           _focusVisible={{ border: "none" }}
+                           _focusVisible={{border: "none"}}
                            {...register("email", {
                                required: "فیلد ضروری میباشد"
                            })}
@@ -54,7 +56,7 @@ const Page = () => {
                     <FormLabel mt='1.5rem' mb='0.5rem' color={color.text.primary} fontSize='1rem'>رمزعبور</FormLabel>
                     <Input placeholder={"رمزعبور خود را وارد نمایید"} color={color.text.secondary} fontSize='1rem'
                            bg={color.base.textField} borderColor={color.base.textFieldStroke}
-                           _focusVisible={{ border: "none" }}
+                           _focusVisible={{border: "none"}}
                            {...register("password", {
                                required: "فیلد ضروری میباشد"
                            })}
@@ -63,13 +65,14 @@ const Page = () => {
                         {errors.password && `${errors.password.message}`}
                     </FormErrorMessage>
                 </FormControl>
-                <Button width="auto" mt="2rem" variant="regularPinkButton" type="submit" isLoading={isLoading}>ورود</Button>
+                <Button width="auto" mt="2rem" variant="regularPinkButton" type="submit"
+                        isLoading={isLoading}>ورود</Button>
             </form>
             <Text mt="1.5rem" fontSize="1rem" fontWeight={400} color={color.text.primary}>
                 عضو نیستید؟{' '}
-                <NextLink href="../register" passHref>
-                    <Link color="pink.500" fontWeight="bold"> ثبت نام</Link>
-                </NextLink>
+
+                <Link href={"/register"} color="pink.500" fontWeight="bold"> ثبت نام</Link>
+
             </Text>
         </>
     );
--- lib/openapi/apiClient.ts
+++ lib/openapi/apiClient.ts
@@ -1,10 +1,15 @@
 import axios from "axios";
 
 
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = "http://185.8.174.74:8090"
+
 axios.interceptors.request.use(
     async (request) => {
         const token = ""
 
+
+        console.log(request, "ggg")
         if (token) {
             request.headers['Authorization'] = `Bearer ${token}`;
         }
@@ -13,5 +18,24 @@ axios.interceptors.request.use(
     (error) => Promise.reject(error)
 );
 
+axios.interceptors.response.use(
+    (response) => {
+        // Get the cookies from the response headers
+        const cookies = response;
+        console.log(cookies, "hhh")
+        // alert("po")
+        // if (cookies) {
+        //     // Set the cookie manually
+        //     cookies.forEach(cookie => {
+        //         document.cookie = cookie; // Set each cookie
+        //     });
+        // }
+
+        return response; // Return the response for further processing
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
 
 export * from "./generated-client"
