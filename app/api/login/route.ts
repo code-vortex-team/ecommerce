702fe9077c7bebcc6f8fcd1d5d3a9f394cb4e5e2
pouria sharifi <sharifi751997@gmail.com>
2024-10-06 18:57:43 +0330

    feat(api):add login api

---
+++ app/api/login/route.ts
@@ -0,0 +1,24 @@
+import {NextRequest, NextResponse} from 'next/server';
+import {UserApi} from "@/lib/openapi/generated-client";
+import {cookies} from "next/headers";
+
+export async function POST(req: NextRequest) {
+    try {
+        const body = await req.json();
+        const res: any = await new UserApi().apiUsersAuthPost(body)
+        if (res.status != 200) {
+            return NextResponse.json({message: 'Login failed'}, {status: 500});
+        }
+        const setCookieHeader = res.headers.get('set-cookie');
+
+        const nextRes = NextResponse.json(res.data, {status: 200});
+        nextRes.headers.set('Set-Cookie', setCookieHeader)
+        const serverCookies = cookies();
+
+
+        return nextRes
+    } catch (e) {
+        return NextResponse.json({message: e.message}, {status: 400});
+
+    }
+}
