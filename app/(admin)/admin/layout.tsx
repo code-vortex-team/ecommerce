a44931752fe08f1bc115a49d30f18470d55b7a45
Ali <alikeramatdev@gmail.com>
2024-10-07 06:53:38 -0700

    fix(admin layout) : Fix layout to ensure compatibility with SideMenu changes

--- app/(admin)/admin/layout.tsx
+++ app/(admin)/admin/layout.tsx
@@ -1,82 +1,15 @@
-'use client'
 import SideMenu from "@/components/sidemenu/SideMenu";
-import { useAppSelector } from "@/lib/redux/hooks";
 import { Flex } from "@chakra-ui/react";
 import React from "react";
-import {
-  AiOutlineHome,
-  AiOutlineShopping,
-  AiOutlineShoppingCart,
-} from "react-icons/ai";
-import { MdFavorite } from "react-icons/md";
 
 interface layoutProps {
   children: React.ReactNode;
 }
-const dropDownItems = {
-  title: "ادمین",
-  list: [
-    {
-      name: "داشبود",
-      url: "/admin/dashboard",
-    },
-    {
-      name: "محصول جدید",
-      url: "/admin/product/create",
-    },
-    {
-      name: "مدیریت کاربران",
-      url: "/admin/users",
-    },
-    {
-      name: "سفارشات",
-      url: "/admin/orders",
-    },
-    {
-      name: "پروفایل",
-      url: "/admin/dashboard",
-    },
-    {
-      name: "خروج از حساب",
-      url: "/admin/logout",
-    },
-  ],
-};
 
 const layout: React.FC<layoutProps> = ({ children }) => {
-  const state = useAppSelector(r => r.basket.list)
-  const sidemenuList = [
-    {
-      title: "داشبورد",
-      pathName: "/admin/dashboard",
-      icon: <AiOutlineHome />,
-      notif: null,
-    },
-    {
-      title: "فروشگاه",
-      pathName: "/shop",
-      icon: <AiOutlineShopping />,
-      notif: null,
-    },
-    {
-      title: "سبد خرید",
-      pathName: "/user/card/checkout",
-      icon: <AiOutlineShoppingCart />,
-      notif: state.length,
-    },
-    {
-      title: "علاقه مندی ها",
-      pathName: "/user/favorite",
-      icon: <MdFavorite />,
-      notif: null,
-    },
-  ];
-
   return (
     <Flex>
-      <SideMenu list={sidemenuList} dropDown={dropDownItems}>
-        {children}
-      </SideMenu>
+      <SideMenu>{children}</SideMenu>
     </Flex>
   );
 };
